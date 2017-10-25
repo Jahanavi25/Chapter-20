a3a5119627040a8f58ae68823bcca1de845af72f
Jahanavi25 <32369086+Jahanavi25@users.noreply.github.com>
2017-10-25 09:08:46 -0500

    Add files via upload

--- GenericMethodTest.java
+++ GenericMethodTest.java
@@ -1,26 +1,43 @@
-package com.exc20_7;
+package com.exc20_6;
 
 public class GenericMethodTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Is 2 is equlas to 2 :"+isEqualTo(2, 2));
-		System.out.println("Is 3 is equlas to 2 :"+isEqualTo(3, 2));
-		System.out.println("Is 'C' is equlas to 'C' :"+isEqualTo('C', 'C'));
-		System.out.println("Is 'D' is equlas to 'C' :"+isEqualTo('D', 'C'));
-		System.out.println("Is 2.4 is equlas to 2.1 :"+isEqualTo(2.4, 2.1));
-		System.out.println("Is 5.0 is equlas to 5.00 :"+isEqualTo(5.0, 5.00));
-		System.out.println("Is \"abc\" is equlas to \"abc\" :"+isEqualTo("abc", "abc"));
-		System.out.println("Is \"abc\" is equlas to \"cba\" :"+isEqualTo("abc", "cba"));
-		Object ob1=new Object();
-		Object ob2=new Object();
-		System.out.println("Is object1 is equlas to object2 :"+isEqualTo(ob1, ob2));
+		Integer[] intArray = {1,2,3,4,5};
+		Double[] doubleArray = {1.1,2.2,3.3,4.4,5.5,6.6,7.7};
+		Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+		String[] stringArray = {"one","two","three","four","five","six","seven","eight"};
+		
+		System.out.println("Array integerArray contains: ");
+		printArray(intArray);
+		System.out.println("Array doubleArray contains: ");
+		printArray(doubleArray);
+		System.out.println("Array characterArray contains: ");
+		printArray(characterArray);
+		System.out.println("Array stringArray contains: ");
+		printArray(stringArray);
+		
 	}
-	 public static <T> boolean isEqualTo(T first, T second)
+	 public static <T> void printArray(T[] inputArray)
 	 {
-		 if(first.equals(second))
-			 return true;
-		 else
-			 return false;
+		 for(T element : inputArray)
+		 {
+			 System.out.printf("%s ",element);
+		 }
+		 System.out.println();
+	 }
+	 public static void printArray(String[] inputArray)
+	 {
+		 int len=inputArray.length;
+		 int i=0;
+		 for(String element : inputArray)
+		 {
+			 System.out.printf("%-10s ",element);
+			 i++;
+			 if(i==len/2)
+				 System.out.println();
+		 }
+		 System.out.println();
 	 }
 }
